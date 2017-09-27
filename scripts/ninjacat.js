2875449f34a2a4fd416bbbbb1d68a826ec045fa0
StephenWeil <stephen.weil@gmail.com>
2017-09-27 16:29:05 -0500

    added a check for winning the game that displays an alert

--- scripts/ninjacat.js
+++ scripts/ninjacat.js
@@ -11,5 +11,11 @@ $(document).ready(function(){
     };
     $('img').click(function() {
         $(this).attr('src', randomImage());
+        if ($('img:first-of-type').attr('src') == "images/ninja0.png" && $('img:nth-of-type(2)').attr('src') == "images/ninja1.png" && $('img:nth-of-type(3)').attr('src') == "images/ninja2.png" && $('img:nth-of-type(4)').attr('src') == "images/ninja3.png" && $('img:last-of-type').attr('src') == "images/ninja4.png") {
+            alert("Congratulations! You won!");
+        }
+        else if ($('img:first-of-type').attr('src') == "images/cat0.png" && $('img:nth-of-type(2)').attr('src') == "images/cat1.png" && $('img:nth-of-type(3)').attr('src') == "images/cat2.png" && $('img:nth-of-type(4)').attr('src') == "images/cat3.png" && $('img:last-of-type').attr('src') == "images/cat4.png") {
+            alert("Congratulations! You won!");
+        }
     });
 });
