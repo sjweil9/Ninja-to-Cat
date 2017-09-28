c95497bd392552fc59c6b3772ceb73889282a595
StephenWeil <stephen.weil@gmail.com>
2017-09-27 21:20:52 -0500

    added some styling and cool functionality - random mode OR ordered slide mode, with message displaying keeping track of wins

--- scripts/ninjacat.js
+++ scripts/ninjacat.js
@@ -9,13 +9,48 @@ $(document).ready(function(){
         }
         return "images/" + type + num + ".png";
     };
+    function nextImage(imgsrc) {
+        if (imgsrc[7] == "n") {
+            var imgnum = parseInt(imgsrc[12]);
+            if (imgnum != 4) {
+                imgnum++;
+                return "images/ninja" + String(imgnum) + ".png";
+            }
+            else {
+                return "images/cat0.png";
+            }
+        }
+        else {
+            var imgnum = parseInt(imgsrc[10]);
+            if (imgnum != 4) {
+                imgnum++;
+                return "images/cat" + String(imgnum) + ".png";
+            }
+            else {
+                return "images/ninja0.png";
+            }
+        }
+    };
+    var wincount = 0;
     $('img').click(function() {
-        $(this).attr('src', randomImage());
+        $(this).fadeTo(400, 0);
+        if ($('input:checked').val() == "Random") {
+            $(this).attr('src', randomImage());
+        }
+        else if ($('input:checked').val() == "Order") {
+            $(this).attr('src', nextImage($(this).attr('src')));            
+        }
+        else {
+            alert("You need to select a game mode!");
+        }
+        $(this).fadeTo(400, 1);
         if ($('img:first-of-type').attr('src') == "images/ninja0.png" && $('img:nth-of-type(2)').attr('src') == "images/ninja1.png" && $('img:nth-of-type(3)').attr('src') == "images/ninja2.png" && $('img:nth-of-type(4)').attr('src') == "images/ninja3.png" && $('img:last-of-type').attr('src') == "images/ninja4.png") {
-            alert("Congratulations! You won!");
+            wincount++;
+            $('p').text("Congratulations! You've won " + String(wincount) + " time(s) now!");
         }
         else if ($('img:first-of-type').attr('src') == "images/cat0.png" && $('img:nth-of-type(2)').attr('src') == "images/cat1.png" && $('img:nth-of-type(3)').attr('src') == "images/cat2.png" && $('img:nth-of-type(4)').attr('src') == "images/cat3.png" && $('img:last-of-type').attr('src') == "images/cat4.png") {
-            alert("Congratulations! You won!");
+            wincount++;
+            $('p').text("Congratulations! You've won " + String(wincount) + " time(s) now!");
         }
     });
 });
